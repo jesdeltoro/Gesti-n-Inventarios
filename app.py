d1594af4de248deb349f1629e640d642e82383cd
jesdeltoro <99795922+jesdeltoro@users.noreply.github.com>
2025-04-25 06:44:31 +0200

    Cambiar debug mode a False

--- app.py
+++ app.py
@@ -191,4 +191,4 @@ def api_eliminar_producto(id):
     return jsonify({'mensaje': 'Producto eliminado exitosamente'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= 5000, debug=True)
+    app.run(host='0.0.0.0', port= 5000, debug=False)
